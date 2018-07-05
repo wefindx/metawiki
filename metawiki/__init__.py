ffbdbab0cf1eff7fe3260577045106da1a8b67eb
Mindey <mindey@qq.com>
2018-07-05 23:06:13 +0000

    add the url->name mapper, and refactor

--- metawiki/__init__.py
+++ metawiki/__init__.py
@@ -5,6 +5,18 @@ import parse
 class MetaWikiError(Exception):
     pass
 
+def convert(name, template, inverse=False):
+
+    if inverse:
+        REVERSED_MAP = {item[-1][0]: [item[0]] for item in MAP.items()}
+        endplate = REVERSED_MAP[template][0]
+    else:
+        endplate = MAP[template][0]
+
+    p = parse.parse(template, name)
+
+    return endplate.format(**p.named)
+
 def name_to_url(name):
 
     template = None
@@ -13,55 +25,56 @@ def name_to_url(name):
     if name.startswith('IN:'):
         if not '#' in name:
             template = 'IN:{user}/{concept}'
-            endplate = MAP[template][0]
-            p = parse.parse(template, name)
-
-            return endplate.format(
-                concept=p['concept'],
-                user=p['user'])
         else:
             template = 'IN:{user}/{concept}#{format}'
-            endplate = MAP[template][0]
-            p = parse.parse(template, name)
-
-            return endplate.format(
-                concept=p['concept'],
-                user=p['user'],
-                format=p['format'])
 
     # Wikidata
     elif name.startswith('WD:'):
         if name.startswith('WD:Q'):
             template = 'WD:Q{integer}'
-            endplate = MAP[template][0]
-            p = parse.parse(template, name)
-
-            return endplate.format(
-                integer=p['integer'])
-
 
         elif name.startswith('WD:P'):
             template = 'WD:P{integer}'
-            endplate = MAP[template][0]
-            p = parse.parse(template, name)
-
-            return endplate.format(
-                integer=p['integer'])
 
         else:
             raise MetaWikiError(
                 "Wikidata concept cannot start with anything other than WD:Q or WD:P, you provided: {}.".format(name)
             )
 
-    if template is None:
+    if template:
+        return convert(name, template)
+    else:
         raise MetaWikiError(
             "Could not find the template for concept: {}.".format(name)
         )
 
-    return
-
 
 def url_to_name(url):
+
+    template = None
+
+    # INDB
     if url.startswith('https://github.com/'):
-        if '/wiki/' in url:
-            pass
+        if '/indb/wiki/' in url:
+            if not '#' in url:
+                template = 'https://github.com/{user}/indb/wiki/{concept}'
+            else:
+                template = 'https://github.com/{user}/indb/wiki/{concept}#{format}'
+        else:
+            raise MetaWikiError("Undefined GitHub namespace for: {}. (currently, only repo=indb in url are valid)".format(url))
+
+    # Wikidata
+    if url.startswith('https://www.wikidata.org/'):
+        if '/wiki/Q' in url:
+            template = 'https://www.wikidata.org/wiki/Q{integer}'
+        elif '/wiki/Property:P' in url:
+            template = 'https://www.wikidata.org/wiki/Property:P{integer}'
+        else:
+            raise MetaWikiError("Undefined Wikidata namespace for: {}.".format(url))
+
+    if template:
+        return convert(url, template, inverse=True)
+    else:
+        raise MetaWikiError(
+            "Could not find the template for url: {}.".format(url)
+        )
--- metawiki/test_map.py
+++ metawiki/test_map.py
@@ -3,22 +3,22 @@ from . import (
     name_to_url
 )
 
-# def test_to_name():
-#
-#     # INDB
-#     result = url_to_name(
-#         'https://github.com/infamily/indb/wiki/percent-change#1hourly')
-#
-#     assert result == 'IN:infamily/percent-change#1hourly'
-#
-#     result =  url_to_name('https://www.wikidata.org/wiki/Q1347367')
-#
-#     # Wikidata
-#     assert result == 'WD:Q/1347367'
-#
-#     result =  url_to_name('https://www.wikidata.org/wiki/Property:P31')
-#
-#     assert result == 'WD:P/31'
+def test_to_name():
+
+    # INDB
+    result = url_to_name(
+        'https://github.com/infamily/indb/wiki/percent-change#1hourly')
+
+    assert result == 'IN:infamily/percent-change#1hourly'
+
+    result =  url_to_name('https://www.wikidata.org/wiki/Q1347367')
+
+    # Wikidata
+    assert result == 'WD:Q/1347367'
+
+    result =  url_to_name('https://www.wikidata.org/wiki/Property:P31')
+
+    assert result == 'WD:P/31'
 
 def test_to_url():
 
